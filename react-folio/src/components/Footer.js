fce0814c2aa0396580cfbe2843184c281ca466f5
akaur091 <amniderkaur.09@gmail.com>
2022-06-01 22:12:37 -0700

    add starter code for footer

--- react-folio/src/components/Footer.js
+++ react-folio/src/components/Footer.js
@@ -1,4 +1,14 @@
 // import react
+import React, {Component} from "react";
 // try to see if footer from bootstarp can be imported like navbar
-// code for footer... make it stick to the bottom of page
 // export footer
+export default class Footer extends Component{
+    render () {
+        return(
+            <React.Fragment>
+                
+            </React.Fragment>
+        )
+    }
+}
+// code for footer... make it stick to the bottom of page
