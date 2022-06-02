544dcecf84eb6f03793f9c3b659fb1fa674564c7
akaur091 <amniderkaur.09@gmail.com>
2022-06-01 23:36:52 -0700

    fix routes in app.js

--- react-folio/src/App.js
+++ react-folio/src/App.js
@@ -1,20 +1,36 @@
 import './App.css';
 import React from 'react';
+// add react router
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import pages
 import Navbar from './components/Navigation';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Project';
+// fix the function to add teh routes correctly to make them load faster
 function App() {
   return (
     <div className="App">
-      {/* add pages*/}
-      <Header />
-      <Navbar />
-      <Projects />
-      <About />
-      <Footer />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+        <Header />
+          <Navbar />
+          <div className='container'>
+           <Routes>
+             <Route 
+              path="/"
+              element= {<About />}
+              />
+              <Route 
+              path="/"
+              element= {<Projects />}
+              />
+           </Routes>
+          </div>
+          <Footer /> 
+        </div>
+      </Router>      
     </div>
   );
 }
