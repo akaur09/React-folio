9e832daf451a72910a8d4f39ab63ceb74ec616d2
akaur091 <amniderkaur.09@gmail.com>
2022-06-01 20:49:22 -0700

    add starter code for nav bar

--- react-folio/src/components/Navigation.js
+++ react-folio/src/components/Navigation.js
@@ -1,9 +1,25 @@
 // import react
-// import react route for Link
-// import react bootstrap for a Navbar
+import React, {Component} from 'react';
+// export navbar 
+export default class Navbar extends Component {
+    render () {
+        return(
+            <React.Fragment>
+                {/* navbar code following react bootstrap */}
+                <Navbar bg = "dark" variant = "dark">
+                    <nav className='me-auto'>
+                        <li href ="#About"> About Me</li>
+                        <li href ="#Project">Projects</li>
+                        <li href ="#Contact">Contact </li>
+                    </nav>
+                </Navbar>
+            </React.Fragment>
+        );
+    }
+}
+// import react bootstrap for a Navbar (maybe)
 // add navbar function
     // add link for About me page
     // add link for project page
     // add link for contact me page
-    // add link for resume
-// export navbar 
+
