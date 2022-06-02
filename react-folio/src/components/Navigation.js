e163f5cf5c35ab7f9835379d239465a7299b2d86
akaur091 <amniderkaur.09@gmail.com>
2022-06-02 11:24:49 -0700

    test to fix nav bar

--- react-folio/src/components/Navigation.js
+++ react-folio/src/components/Navigation.js
@@ -1,20 +1,33 @@
 // import react
 import React from 'react';
-// export navbar 
-
+// import nav bar from bootstrap
+import { Navbar } from 'react-bootstrap';
+// import Nav section from bootstarp
+import { Nav } from 'react-bootstrap';
 // import react bootstrap for a Navbar
-function Navbar(){
-    return (
-        <nav>
-            <li href ="#About"> About Me</li>
-            <li href ="#Project">Projects</li>
-            <li href ="#Contact">Contact </li>
-        </nav>
-    )
-}
 // add navbar function
     // add link for About me page
     // add link for project page
     // add link for contact me page
     // add link for resume
+function Navbar(){
+    // add a scroll effect
+    function scrollHandler(){
+        if (window.scrollY >= 20){
+            updateNavbar(true);
+        } else {
+            updateNavbar(false);
+        }
+    }
+    window.addEventListener("scroll", scrollHandler);
+    return (
+        // add navbar css 
+        <Nav>
+            <li href ="#About"> About Me</li>
+            <li href ="#Project">Projects</li>
+            <li href ="#Contact">Contact </li>
+        </Nav>
+    )
+}
+// export navbar
 export default Navbar;
