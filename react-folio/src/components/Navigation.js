95a377c1814aab4691f29a84d7a9627d116b6460
akaur091 <amniderkaur.09@gmail.com>
2022-06-01 23:19:16 -0700

    remove react.fragment function

--- react-folio/src/components/Navigation.js
+++ react-folio/src/components/Navigation.js
@@ -1,25 +1,20 @@
 // import react
-import React, {Component} from 'react';
+import React from 'react';
 // export navbar 
-export default class Navbar extends Component {
-    render () {
-        return(
-            <React.Fragment>
-                {/* navbar code following react bootstrap */}
-                <Navbar bg = "dark" variant = "dark">
-                    <nav className='me-auto'>
-                        <li href ="#About"> About Me</li>
-                        <li href ="#Project">Projects</li>
-                        <li href ="#Contact">Contact </li>
-                    </nav>
-                </Navbar>
-            </React.Fragment>
-        );
-    }
-}
+
 // import react bootstrap for a Navbar
+function Navbar(){
+    return (
+        <nav>
+            <li href ="#About"> About Me</li>
+            <li href ="#Project">Projects</li>
+            <li href ="#Contact">Contact </li>
+        </nav>
+    )
+}
 // add navbar function
     // add link for About me page
     // add link for project page
     // add link for contact me page
     // add link for resume
+export default Navbar;
