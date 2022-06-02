099c6cb852883f37ccb0b7300d0f1f0d4afd6c53
akaur091 <amniderkaur.09@gmail.com>
2022-06-01 20:01:52 -0700

    add code to import react in header.js

--- react-folio/src/components/Header.js
+++ react-folio/src/components/Header.js
@@ -1,5 +1,7 @@
 // import react
+import React, { Component} from "react";
 // import react route
+
 // import navigation, projects, footer about me and contact me page
 // code for header 
     // render navigation page
