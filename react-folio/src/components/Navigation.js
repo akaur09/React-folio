343967be150914bf58bf48f1a279bfb7e77ca411
akaur091 <amniderkaur.09@gmail.com>
2022-06-01 23:02:22 -0700

    attpemt to fix navbar

--- react-folio/src/components/Navigation.js
+++ react-folio/src/components/Navigation.js
@@ -17,9 +17,9 @@ export default class Navbar extends Component {
         );
     }
 }
-// import react bootstrap for a Navbar (maybe)
+// import react bootstrap for a Navbar
 // add navbar function
     // add link for About me page
     // add link for project page
     // add link for contact me page
-
+    // add link for resume
