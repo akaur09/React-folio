18c62fe1e3f9641e05349902289decb304b129b7
akaur091 <amniderkaur.09@gmail.com>
2022-06-01 21:17:15 -0700

    import react and set up Projects component

--- react-folio/src/components/Project.js
+++ react-folio/src/components/Project.js
@@ -1,5 +1,14 @@
 // import react
+import React, {Component} from "react";
 // import react route (???? maybe)
-// import bootsrtap maybe
+export default class Projects extends Component {
+   render () {
+       return (
+           <React.Fragment>
+               
+           </React.Fragment>
+       );
+   }
+}
 // add functions for project cards... (3-4 project cards)
 // export project
