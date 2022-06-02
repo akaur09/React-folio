42589ccd29754220116198a8a994cb4755877b2f
akaur091 <amniderkaur.09@gmail.com>
2022-06-01 22:41:43 -0700

    fix spelling mistake

--- react-folio/src/App.js
+++ react-folio/src/App.js
@@ -9,7 +9,7 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      {/* add pages */}
+      {/* add pages*/}
       <Header />
       <Navbar />
       <Projects />
